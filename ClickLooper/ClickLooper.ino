215df50631f44fcf7bf298fd4d6558dab3b46831
Hirofumi Matsushima <ma2cma@i.117.cx>
2018-04-22 10:56:50 +0900

    新規

---
+++ ClickLooper/ClickLooper.ino
@@ -0,0 +1,39 @@
+// https://digistump.com/board/index.php?topic=2540.0
+
+#include "DigiMouse.h"
+
+const int moveOffset = 67;
+const int duration = 30000; // msec 180000:3min
+const int ledPin = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT); //LED on Model A  
+  DigiMouse.begin(); //start or reenumerate USB
+}
+
+void loop() {
+  DigiMouse.update(); // DigiMouse.update() at least every 50ms
+
+  DigiMouse.delay(500);  
+  DigiMouse.setButtons(1); //left click
+  digitalWrite(ledPin, true);  
+
+  DigiMouse.delay(500);
+  DigiMouse.setButtons(0); //unclick all
+  digitalWrite(ledPin, false);    
+
+  DigiMouse.delay(500);
+  DigiMouse.moveX(moveOffset); 
+
+  DigiMouse.delay(500);  
+  DigiMouse.setButtons(1); //left click
+  digitalWrite(ledPin, true);  
+  
+  DigiMouse.delay(500);
+  DigiMouse.setButtons(0); //unclick all
+  digitalWrite(ledPin, false);    
+
+  DigiMouse.delay(duration); 
+  DigiMouse.moveX(-moveOffset); 
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ClickLooper
+
+## プログラム説明
+
+USBセット後に、マウスを現在の位置から数ピクセル（固定値）左に移動した後にクリックし再び元の位置に移動後にクリックます。この動作を30秒ごとに繰り返します。
+
+## 開発関連情報
+
+- [https://www.arduino.cc/en/Main/Software Arduino 開発環境]
+
+- [http://digistump.com/wiki/digispark/tutorials/basics Dispark 関連]
+
+- [http://denshibu.blog.fc2.com/blog-entry-80.html 使用された方の個人ブログ]
